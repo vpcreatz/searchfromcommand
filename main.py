9a9e9abdabbd27789da2b2f61f0080a4f194b28a
vpcreatz <119391455+vpcreatz@users.noreply.github.com>
2023-02-19 22:12:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sqlite3
 import pymongo
 from pyrogram import Client, filters
 from telegram.ext import CommandHandler
+from database import dispatcher.add_handler
 
 API_ID = "20960397"
 API_HASH = "d68d847d3abb2087bf74f5d0683c2993"
